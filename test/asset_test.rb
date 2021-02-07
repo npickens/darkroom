ea36666d98ba1669aadb0f73d659b05912190c70
Nate Pickens <nate@pickens.email>
2021-02-07 07:09:19 -0800

    Add tests for Darkroom::Asset#internal?

--- test/asset_test.rb
+++ test/asset_test.rb
@@ -1,3 +1,30 @@
 class AssetTest < Minitest::Test
-  # Comming soon.
+  describe('Darkroom') do
+    describe('Asset') do
+      before do
+        @hello_file = File.expand_path(File.join('test', 'assets', 'hello.txt'))
+        @hello_path = '/hello.txt'
+      end
+
+      describe('#internal?') do
+        it('returns true if asset was initialized as internal') do
+          asset = Darkroom::Asset.new(@hello_path, @hello_file, {}, internal: true)
+
+          assert(asset.internal?)
+        end
+
+        it('returns false if asset was initialized as non-internal') do
+          asset = Darkroom::Asset.new(@hello_path, @hello_file, {}, internal: false)
+
+          refute(asset.internal?)
+        end
+
+        it('returns false if asset was initialized without specifying internal status') do
+          asset = Darkroom::Asset.new(@hello_path, @hello_file, {})
+
+          refute(asset.internal?)
+        end
+      end
+    end
+  end
 end
