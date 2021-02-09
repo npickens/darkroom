9994ae569a2e7a525306639a0c791b941f1f3b73
Nate Pickens <nate@pickens.email>
2021-02-09 09:10:46 -0800

    Clean up tests and run setup only once instead of before each test

--- test/asset_test.rb
+++ test/asset_test.rb
@@ -1,30 +1,20 @@
 class AssetTest < Minitest::Test
   describe('Darkroom') do
     describe('Asset') do
-      before do
-        @hello_file = File.expand_path(File.join('test', 'assets', 'hello.txt'))
-        @hello_path = '/hello.txt'
+      ######################################################################################################
+      ## Setup                                                                                            ##
+      ######################################################################################################
+
+      begin
+        ASSET_DIR = File.expand_path(File.join('test', 'assets'))
+
+        HELLO_PATH = '/hello.txt'
+        HELLO_FILE = File.join(ASSET_DIR, HELLO_PATH)
       end
 
-      describe('#internal?') do
-        it('returns true if asset was initialized as internal') do
-          asset = Darkroom::Asset.new(@hello_path, @hello_file, {}, internal: true)
-
-          assert(asset.internal?)
-        end
-
-        it('returns false if asset was initialized as non-internal') do
-          asset = Darkroom::Asset.new(@hello_path, @hello_file, {}, internal: false)
-
-          refute(asset.internal?)
-        end
-
-        it('returns false if asset was initialized without specifying internal status') do
-          asset = Darkroom::Asset.new(@hello_path, @hello_file, {})
-
-          refute(asset.internal?)
-        end
-      end
+      ######################################################################################################
+      ## Asset#headers                                                                                    ##
+      ######################################################################################################
 
       describe('#headers') do
         it('includes correct content type') do
@@ -37,7 +27,7 @@ class AssetTest < Minitest::Test
         end
 
         it('includes Cache-Control header if :versioned is not specified') do
-          asset = Darkroom::Asset.new(@hello_path, @hello_file, {})
+          asset = Darkroom::Asset.new(HELLO_PATH, HELLO_FILE, {})
           headers = asset.headers
 
           assert_equal('public, max-age=31536000', headers['Cache-Control'])
@@ -45,7 +35,9 @@ class AssetTest < Minitest::Test
         end
 
         it('includes Cache-Control header if :versioned is true') do
-          asset = Darkroom::Asset.new(@hello_path, @hello_file, {})
+          asset = Darkroom::Asset.new(HELLO_PATH, HELLO_FILE, {})
+          asset.process(Time.now.to_f)
+
           headers = asset.headers(versioned: true)
 
           assert_equal('public, max-age=31536000', headers['Cache-Control'])
@@ -53,13 +45,37 @@ class AssetTest < Minitest::Test
         end
 
         it('includes ETag header if :versioned is false') do
-          asset = Darkroom::Asset.new(@hello_path, @hello_file, {})
+          asset = Darkroom::Asset.new(HELLO_PATH, HELLO_FILE, {})
           asset.process(Time.now.to_f)
 
           headers = asset.headers(versioned: false)
 
           assert_equal('"09f7e02f1290be211da707a266f153b3"', headers['ETag'])
           assert_nil(headers['Cache-Control'])
+        end
+      end
+
+      ######################################################################################################
+      ## Asset#internal?                                                                                  ##
+      ######################################################################################################
+
+      describe('#internal?') do
+        it('returns true if asset was initialized as internal') do
+          asset = Darkroom::Asset.new(HELLO_PATH, HELLO_FILE, {}, internal: true)
+
+          assert(asset.internal?)
+        end
+
+        it('returns false if asset was initialized as non-internal') do
+          asset = Darkroom::Asset.new(HELLO_PATH, HELLO_FILE, {}, internal: false)
+
+          refute(asset.internal?)
+        end
+
+        it('returns false if asset was initialized without specifying internal status') do
+          asset = Darkroom::Asset.new(HELLO_PATH, HELLO_FILE, {})
+
+          refute(asset.internal?)
         end
       end
     end
